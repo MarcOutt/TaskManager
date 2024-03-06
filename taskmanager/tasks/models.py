187cd2dc46d0ac5caa23859768a8260f524c3d80
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2024-03-06 14:52:48 +0100

    add task model

---
+++ taskmanager/tasks/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
