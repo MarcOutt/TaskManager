d03075fb15830f45c05db389b287dba0953da7e0
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2024-03-11 22:19:34 +0100

    update UserSignupView

--- taskmanager/user/views.py
+++ taskmanager/user/views.py
@@ -1,8 +1,10 @@
+
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.views import View
 from .forms import UserCreationForm, UserLoginForm
+from .models import CustomUser
 
 
 class UserSignupView(View):
@@ -13,30 +15,40 @@ class UserSignupView(View):
     def post(self, request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()  # Sauvegarde l'utilisateur créé par le formulaire
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Bienvenue, {username} ! Votre compte a été créé avec succès. Vous pouvez maintenant vous connecter.')
-            return redirect('user_login')
+            messages.success(request,
+                             f'Bienvenue, {username} ! Votre compte a été créé avec succès. Vous pouvez maintenant vous connecter.')
+
+            # Connexion automatique de l'utilisateur après la création du compte
+            login(request, user)
+            return redirect('home')  # Redirection vers une autre vue après l'inscription
         return render(request, 'user/user_signup.html', {'form': form})
 
 
 class UserLoginView(View):
+    """Permet d'afficher la page de connexion et le lien d'inscription"""
+    template_name = 'user/user_login.html'
+    form_class = UserLoginForm
+
     def get(self, request):
-        form = UserLoginForm()
-        return render(request, 'user/user_login.html', {'form': form})
+        form = self.form_class()
+        message = ''
+        return render(request, self.template_name, context={'form': form, 'message': message})
 
     def post(self, request):
-        form = UserLoginForm(request.POST)
+        form = self.form_class(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=username, password=password)
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'],
+                                )
+            print(user)
             if user is not None:
                 login(request, user)
-                return redirect('user_profile')
-            else:
-                messages.error(request, 'Identifiants incorrects. Veuillez réessayer.')
-        return render(request, 'user/user_login.html', {'form': form})
+                return redirect('home')
+        message = 'Identifiants invalides.'
+        user = False
+        return render(request, self.template_name, context={'form': form, 'message': message, 'user': user})
 
 
 def user_logout(request):
